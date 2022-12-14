0d3a430b82a2d3e0414bdf17cac64d4c38e852b3
chavdar <chavmom@gmail.com>
2022-12-14 22:43:12 +0200

    Changes to be committed: 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = Vue.createApp({
     data() {
         return {
             firstName: 'John',
+            lastName: 'Doe',
+            email: 'John@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
 
     },
